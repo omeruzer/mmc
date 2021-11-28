c3d720dd0fb3c518e27c0407ad4617837515daaa
omeruzer <omeruzer1021@gmail.com>
2021-11-28 12:04:06 +0300

    Register Sayfası kaldırıldı login ve register tek sayfa oldu

--- app/Http/Controllers/admin/SeoController.php
+++ app/Http/Controllers/admin/SeoController.php
@@ -14,28 +14,14 @@ class SeoController extends Controller
     public function index(){
 
         $loginSeo       =   LoginSeo::find(1);
-        $registerSeo    =   RegisterSeo::find(1);
         $blogSeo        =   BlogSeo::find(1);
         $sssSeo         =   SssSeo::find(1);
 
-        return view('admin.seo.seo',compact('loginSeo','registerSeo','blogSeo','sssSeo'));
+        return view('admin.seo.seo',compact('loginSeo','blogSeo','sssSeo'));
     }
-
     public function login(){
 
         $login = LoginSeo::find(1)->update([
-           'keyw' => request('keyw'),
-           'desc' => request('desc'),
-        ]);
-
-        if($login){
-            return redirect()->route('admin.seo')->with('message','İşlem Başarıyla Gerçekleşti')->with('message_type','success');
-        }
-
-    }
-    public function register(){
-
-        $login = RegisterSeo::find(1)->update([
            'keyw' => request('keyw'),
            'desc' => request('desc'),
         ]);
--- resources/views/admin/seo/seo.blade.php
+++ resources/views/admin/seo/seo.blade.php
@@ -51,31 +51,6 @@
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
             <div class="card mb-3">
                 <div class="card-header">
-                    <h3><i class="far fa-check-square"></i> Register SEO</h3>
-                </div>
-                <div class="card-body">
-                    <form action="{{route('admin.seo.register')}}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email">Anahtar Kelime</label>
-                            <input type="text" name="keyw" value="{{$registerSeo->keyw}}" class="form-control" placeholder="Anahtar Kelime" >
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Açıklama</label>
-                            <input type="text" name="desc"value="{{$registerSeo->desc}}"  class="form-control" placeholder="Açıklama" >
-                        </div>
-                        <div class="" style="float: right;">
-                        <button type="submit" class="btn btn-primary">Kaydet</button>
-                        </div>
-                    </form>
-        
-                </div>
-            </div><!-- end card-->
-        </div>
-
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-            <div class="card mb-3">
-                <div class="card-header">
                     <h3><i class="far fa-check-square"></i> SSS SEO</h3>
                 </div>
                 <div class="card-body">
